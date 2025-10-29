13713524dad77b83f5b56cbf27599dff48bd3e45
Rihabdel <rihababdel@gmail.com>
2025-10-29 11:35:08 +0100

    creer la structure de projet et le header et footer dans views/index.php

---
+++ app/views/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/scss/main.css">
+
+    <title>Ecoride</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/home">EcoRide</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Accès Covoiturage</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/inscription">Inscription</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main id="main-page">
+        <button class="btn btn-primary">Bouton Exemple</button>
+
+    </main>
+    <footer>
+        <div class="bg-dark text-white text-center p-3 footer">
+            <div class="row">
+                <div class="col-6 me-auto col-lg-4">
+                    <h3 class="text-primary">Emails</h3>
+                    <p>contact@ecoride.com</p>
+                </div>
+                <div class="col-6 ms-auto col-lg-4">
+                    <h3 class="text-primary">Mentions Légales</h3>
+                    <p>www.ecoride.com/mentionlegale</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
